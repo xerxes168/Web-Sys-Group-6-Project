b7084135f8d2cd96c45830f95e87f92370dc8625
xerxes168 <2402253@sit.singaporetech.edu.sg>
2025-03-07 14:55:29 +0800

    Created index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Sys Group 6 Project</title>
+</head>
+<body>
+    <h1>Welcome to Web Sys Group 6 Project</h1>
+    <p>This is the homepage of our project.</p>
+</body>
+</html>
