b2b209abcceaff542845a24501251fa6265c800b
Alexi George <wolfverusofficial@gmail.com>
2025-03-30 06:52:19 +0800

    Bootstrap + remove sit_login.css conflicts

--- login.php
+++ login.php
@@ -33,28 +33,34 @@
 
         <!-- Login Section -->
         <section class="login-section" id="top">
-            <div class="login-form">
-                <h2 style="text-align: center; margin-bottom: 20px;">Log In to GatherSpot</h2>
-                <div class="line-dec" style="width: 50px; height: 3px; background: #ff589e; margin: 0 auto 20px;"></div>
-                
-                <!-- Login Form -->
-                <form action="process_login.php" method="POST">
-                    <div class="form-group">
-                        <label for="username">Username or Email</label>
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username or email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
-                    </div>
-                    <div class="form-group" style="text-align: right;">
-                        <a href="#" style="color: #ff589e;">Forgot Password?</a>
-                    </div>
-                    <button type="submit" class="btn btn-primary" style="width: 100%; background: #ff589e; border: none; padding: 12px;">Log In</button>
-                </form>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
+                        <div class="login-form">
+                            <h2 style="text-align: center; margin-bottom: 20px;">Log In to GatherSpot</h2>
+                            <div class="line-dec" style="width: 50px; height: 3px; background: #ff589e; margin: 0 auto 20px;"></div>
+                            
+                            <!-- Login Form -->
+                            <form action="process_login.php" method="POST">
+                                <div class="form-group">
+                                    <label for="username">Username or Email</label>
+                                    <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username or email" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
+                                </div>
+                                <div class="form-group" style="text-align: right;">
+                                    <a href="#" style="color: #ff589e;">Forgot Password?</a>
+                                </div>
+                                <button type="submit" class="btn btn-primary" style="width: 100%; background: #ff589e; border: none; padding: 12px;">Log In</button>
+                            </form>
 
-                <!-- Sign Up Prompt -->
-                <p style="text-align: center; margin-top: 20px;">Don't have an account? <a href="register.php" style="color: #ff589e;">Sign Up</a></p>
+                            <!-- Sign Up Prompt -->
+                            <p style="text-align: center; margin-top: 20px;">Don't have an account? <a href="register.php" style="color: #ff589e;">Sign Up</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </body>
