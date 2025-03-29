0368dc9dc0edac868aabc4e06f5436de42a7df4a
Alexi George <wolfverusofficial@gmail.com>
2025-03-30 07:21:00 +0800

    Update color scheme + Fix WCAG
    Left with adding a h1 tag

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>GatherSpot - Log In</title>
+        <title>Log In</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -11,10 +11,6 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/fontAwesome.css">
-        <link rel="stylesheet" href="css/hero-slider.css">
-        <link rel="stylesheet" href="css/owl-carousel.css">
-        <link rel="stylesheet" href="css/datepicker.css">
-        <link rel="stylesheet" href="css/templatemo-style.css">
         <link rel="stylesheet" href="css/sit_login.css">
 
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
@@ -36,7 +32,7 @@
                     <div class="row">
                         <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
                             <div class="login-form">
-                                <h2 style="text-align: center; margin-bottom: 20px;">Log In to GatherSpot</h2>
+                                <h2 style="text-align: center; margin-bottom: 20px;">Login</h2>
                                 <div class="line-dec" style="width: 50px; height: 3px; background: #ff589e; margin: 0 auto 20px;"></div>
                                 
                                 <!-- Login Form -->
@@ -49,14 +45,11 @@
                                         <label for="password">Password</label>
                                         <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
                                     </div>
-                                    <div class="form-group" style="text-align: right;">
-                                        <a href="#" style="color: #ff589e;">Forgot Password?</a>
-                                    </div>
                                     <button type="submit" class="btn btn-primary" style="width: 100%; background: #ff589e; border: none; padding: 12px;">Log In</button>
                                 </form>
 
                                 <!-- Sign Up Prompt -->
-                                <p style="text-align: center; margin-top: 20px;">Don't have an account? <a href="register.php" style="color: #ff589e;">Sign Up</a></p>
+                                <p style="text-align: center; margin-top: 20px;">Don't have an account? <a class="login_link" href="register.php">Sign Up</a></p>
                             </div>
                         </div>
                     </div>
--- register.php
+++ register.php
@@ -3,11 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>GatherSpot - Register</title>
-        <!-- 
-        Avalon Template 
-        http://www.templatemo.com/tm-513-avalon
-        -->
+        <title>Register</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -15,10 +11,6 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/fontAwesome.css">
-        <link rel="stylesheet" href="css/hero-slider.css">
-        <link rel="stylesheet" href="css/owl-carousel.css">
-        <link rel="stylesheet" href="css/datepicker.css">
-        <link rel="stylesheet" href="css/templatemo-style.css">
         <link rel="stylesheet" href="css/sit_register.css">
 
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
@@ -40,7 +32,7 @@
                     <div class="row">
                         <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
                             <div class="register-form">
-                                <h2 style="text-align: center; margin-bottom: 20px;">Register for GatherSpot</h2>
+                                <h2 style="text-align: center; margin-bottom: 20px;">Register</h2>
                                 <div class="line-dec" style="width: 50px; height: 3px; background: #ff589e; margin: 0 auto 20px;"></div>
                                 
                                 <!-- Registration Form -->
@@ -65,11 +57,11 @@
                                         <label for="pwd_confirm">Confirm Password</label>
                                         <input type="password" class="form-control" id="pwd_confirm" name="pwd_confirm" placeholder="Confirm your password" required>
                                     </div>
-                                    <button type="submit" class="btn btn-primary" style="width: 100%; background: #ff589e; border: none; padding: 12px;">Register</button>
+                                    <button type="submit" class="btn btn-primary" style="width: 100%; background: #5f52b0; border: none; padding: 12px;">Register</button>
                                 </form>
 
                                 <!-- Login Prompt -->
-                                <p style="text-align: center; margin-top: 20px;">Already have an account? <a href="login.php" style="color: #ff589e;">Log In</a></p>
+                                <p style="text-align: center; margin-top: 20px;">Already have an account? <a class="login_link" href="login.php">Log In</a></p>
                             </div>
                         </div>
                     </div>
