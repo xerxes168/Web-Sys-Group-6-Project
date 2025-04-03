40bf706d4b513d0e62aed94c312909537ff5cbf5
Levetii <crazymindsxd@gmail.com>
2025-04-03 18:58:38 +0800

    remove duplicate code
    accidentally add 2

--- login.php
+++ login.php
@@ -18,9 +18,6 @@
 
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
         <script src="js/sit_validator.js"></script>
-        <?php 
-        include "inc/head.inc.php"; 
-        ?> 
     </head>
 
     <body>
