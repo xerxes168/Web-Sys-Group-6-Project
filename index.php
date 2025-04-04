a37eaee35b50f2ae1c64ce21d82a5aabde6770aa
Elsia Teo Yu Ning <2402415@sit.singaporetech.edu.sg>
2025-04-04 21:56:19 +0800

    remove contact section

--- index.php
+++ index.php
@@ -164,40 +164,6 @@ http://www.templatemo.com/tm-513-avalon
         </div>
     </section>
 
-    <section class="contact-us" id="contact-section"> 
-        <div class="container-fluid">
-            <div class="row">
-                <form id="contact" action="" method="post">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <fieldset>
-                            <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
-                            </fieldset>
-                        </div>
-                        <div class="col-md-4">
-                            <fieldset>
-                            <input name="email" type="email" class="form-control" id="email" placeholder="Your email..." required="">
-                            </fieldset>
-                        </div>
-                            <div class="col-md-4">
-                            <fieldset>
-                            <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject..." required="">
-                            </fieldset>
-                        </div>
-                        <div class="col-md-12">
-                            <fieldset>
-                            <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
-                            </fieldset>
-                        </div>
-                        <div class="col-md-6">
-                            <fieldset>
-                            <button type="submit" id="form-submit" class="btn">Send</button>
-                            </fieldset>
-                        </div>
-                    </form>
-                </div>
-    </section>
-
 <section class="contact-details" id="contact-details-section">
   <div class="container-fluid">
     <div class="contact-details-container">
